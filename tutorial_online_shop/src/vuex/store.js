3242ba91a4d9d5de5f89707d6383e9c6a3ea85cb
Mariia2509 <maria2509@list.ru>
2023-02-14 00:17:38 +0300

    Настроить динамику кнопок Добавить стили для кнопок каталога

--- tutorial_online_shop/src/vuex/store.js
+++ tutorial_online_shop/src/vuex/store.js
@@ -1,51 +1,6 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
 
-const fakeProducts = [
-  {
-    "image": "1.jpg",
-    "name": "T-shirt 1",
-    "price": 100,
-    "article": "T1",
-    "available": true
-  },
-  {
-    "image": "2.jpg",
-    "name": "T-shirt 2",
-    "price": 150,
-    "article": "T2",
-    "available": true
-  },
-  {
-    "image": "3.jpg",
-    "name":"T-shirt 3",
-    "price": 200,
-    "article": "T3",
-    "available": false
-  },
-  {
-    "image": "4.jpg",
-    "name": "T-shirt 4",
-    "price": 250,
-    "article": "T4",
-    "available": true
-  },
-  {
-    "image": "5.jpg",
-    "name": "T-shirt 5",
-    "price": 300,
-    "article": "T5",
-    "available": false
-  },
-  {
-    "image": "6.jpg",
-    "name": "T-shirt 6",
-    "price": 350,
-    "article": "T6",
-    "available": true
-  }
-]
-
 // Create a new store instance.
 const store = createStore({
   state() {
@@ -87,8 +42,6 @@ const store = createStore({
   },
   actions: {
     GET_PRODUCTS_FROM_API({ commit }) {
-      commit('SET_PRODUCTS_TO_STATE', fakeProducts)
-// /*
       return axios('http://localhost:3000/products', {
         method: 'GET'
       })
